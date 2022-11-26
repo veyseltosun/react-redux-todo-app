741d75fbd649e98aa025f7c6992a05ccebc23b56
veyseltosun <postalarim1923@gmail.com>
2022-11-26 19:39:24 +0300

    v2.

--- todo/src/redux/store.js
+++ todo/src/redux/store.js
@@ -1,8 +1,8 @@
 export const INITIAL_STATE = {
     counter: 0,
     list: [
-        { id: 0, text: "Kedinin yemini kontrol et!", completed: false },
-        { id: 1, text: "Manava git!", completed: false },
-        { id: 2, text: "Projeye devam et!", completed: false },
+        { id: 0, text: "Atışları yap !!", completed: false },
+        { id: 1, text: "Boş kovanları topla!!", completed: false },
+        { id: 2, text: "Yeşil donunu giy Homeke!", completed: false },
        ],
   };
